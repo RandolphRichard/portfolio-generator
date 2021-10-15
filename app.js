5d1feca4c87788bfdacdde5c14f34eba1a1f70ef
Randolph Richard <randolphrichard9@gmail.com>
2021-10-15 00:11:41 -0400

    structuring the html with .map() and .filter()

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -149,6 +149,12 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
 
